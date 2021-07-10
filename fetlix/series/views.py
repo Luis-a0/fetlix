2356aae068b9ccb21b5431972ba9cb94cb7d4bed
VoidCanvas <lumaaviles0@gmail.com>
2021-07-10 17:48:29 -0500

    Modificados y agregados elementos para la visualizacion de registros mediante los templates

--- fetlix/series/views.py
+++ fetlix/series/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import View
 
-class HelloWorld(View):
+from series.models import Serie, Episode
+
+class SerieView(View):
 	
 	def get(self, request):
 		context = {
-			'items': list(range(10))
+			'series': list(Serie.objects.all())
 		}
-		return render(request, 'index.html', context=context)
+		return render(request, 'series.html', context=context)
+
+class EpisodeView(View):
+	
+	def get(self, request, serie_id: int):
+		context = {
+			'episodes': list(Episode.objects.filter(serie_id=serie_id))
+		}
+		return render(request, 'episodes.html', context=context)
 		
